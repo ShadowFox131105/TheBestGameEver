b8d35ba1df90d00d63d9bba45fb5bef718b261cb
ShadowFox131105 <dima.sv.1311200593@gmail.com>
2024-02-05 19:04:25 +0300

    Добавил персонажа с материалом и написал скрипт кручения

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraRotation : MonoBehaviour
+{
+
+    public float RotationSpeed = 1f;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed, 0);
+    }
+}
