f1a218748ca2e5ed7fd3cc820cabea62c2343bde
ShadowFox131105 <dima.sv.1311200593@gmail.com>
2024-02-06 16:26:53 +0300

    Добавил возможность управлять персонажем при помощи мыши

--- Assets/Scripts/CameraRotation.cs
+++ Assets/Scripts/CameraRotation.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
-
+    public Transform CameraAxisTransform;
     public float RotationSpeed = 1f;
+    public float RotationSpeedY = 1f;
+    public float minAngle; 
+    public float maxAngle;
     void Start()
     {
         
@@ -14,6 +17,19 @@ public class CameraRotation : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed, 0);
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime * RotationSpeed * Input.GetAxis("Mouse X"), 0);
+
+        var newAngleX = CameraAxisTransform.localEulerAngles.x + Time.deltaTime * RotationSpeedY * -Input.GetAxis("Mouse Y");
+        
+        if (newAngleX > 180)
+        {
+            newAngleX -= 360;
+        }
+
+        newAngleX = Mathf.Clamp(newAngleX, minAngle, maxAngle);
+
+
+
+        CameraAxisTransform.localEulerAngles = new Vector3 (newAngleX, 0,0);
     }
 }
